0cd7f1575665401d861ce0121becc3e744c4f6cc
Lens Mutombo <lensmutombo@gmail.com>
2019-08-17 11:23:54 +0200

    First commit project
    DonielApp est une app de petites annonces entre particuliers

---
+++ app/src/main/java/com/conebase/donielapp/Activities/RegisterActivity.java
@@ -0,0 +1,80 @@
+package com.conebase.donielapp.Activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.conebase.donielapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageView userPhoto;
+    static int PreqCode = 1;
+    static int REQUESCODE = 1;
+    Uri pickedImgUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        userPhoto = findViewById(R.id.imgv_user);
+        userPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 22) {
+                    checkAndRequestForPermission();
+                } else {
+                    OpenGallery();
+                }
+            }
+        });
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK && requestCode==REQUESCODE && data !=null){
+            pickedImgUri=data.getData();
+            userPhoto.setImageURI(pickedImgUri);
+        }
+
+    }
+
+    private void checkAndRequestForPermission() {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(RegisterActivity.this, "Veuillez valider la permission", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(RegisterActivity.this, new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, PreqCode);
+            }
+        } else {
+            OpenGallery();
+        }
+
+
+    }
+}
+
