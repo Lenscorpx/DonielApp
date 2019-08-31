d07a1b92e6e3e3c56064e3707ba40f153674c55f
Lens Mutombo <lensmutombo@gmail.com>
2019-08-31 04:28:54 +0200

    splashscreen added

---
+++ app/src/main/java/com/conebase/donielapp/Activities/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.conebase.donielapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.conebase.donielapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
